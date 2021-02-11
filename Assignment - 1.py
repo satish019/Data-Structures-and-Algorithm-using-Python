0cb2941b824daf4d2811fe810d42432beb1cc74b
Satish <bokkasatish019@gmail.com>
2021-02-11 23:31:54 +0530

    Add files via upload

---
+++ Assignment - 1.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Assignment - 1
+
+# # Question 1
+
+# # Write a python program that takes two numbers as the input and print the result of Addition, Subtraction, Multiplication, Division of the two numbers.
+
+# In[1]:
+
+
+num1 = int(input("Enter 1st Number :"))
+num2 = int(input("Enter 2nd Number :"))
+
+add = num1 + num2
+print("Addition is :",add)
+
+sub = num1 - num2
+print("Subtraction is :",sub)
+
+mul = num1 * num2
+print("Multiplication is :",mul)
+
+div = num1 / num2
+print("Division is :",div)
+
+
+# # Question 2
+
+# # Write a python program that takes two numbers as the input such as X and Y and print the result of X^Y(X to the power of Y).
+
+# In[2]:
+
+
+X = int(input("Enter the number :"))
+Y = int(input("Enter the power of number:"))
+
+power = X**Y
+print(X,"to the power of",Y,"is",power)
+
+
+# In[ ]:
+
+
+
+
