67298221802eef378084b97fffc5c85394a1250f
Satish <bokkasatish019@gmail.com>
2021-02-11 23:36:02 +0530

    Add files via upload

---
+++ Assignment-3.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Assignment - 3
+
+# # Question 1
+
+# # Create an Array in python using the list and perform delete from any position operation.
+
+# # Solution:
+
+# In[1]:
+
+
+arr_list = [10, 15, 20, 30, 5, 25]
+print(arr_list)
+arr_list.remove(15)
+print(arr_list)
+
+
+# # Question 2
+
+# # What is constructor? Mention the task of the constructor.¶
+
+# # Answer :
+
+# Constructors are generally used for instantiating an object.
+# The task of constructors is to initialize(assign values) to the data members of the class when an object of class is created.
+# In Python the __init__() method is called the constructor and is always called when an object is created.
+# 
+
+# # Syntax of constructor declaration :
+
+# def __init__(self):
+#     # body of the constructor
+
+# In[ ]:
+
+
+
+
